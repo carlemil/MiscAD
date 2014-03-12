3bfd93a7f09b8ee9cbe4bd517286ff53be2bab75
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-03-12 14:26:37 +0100

    added bst find.

--- BST/test/se/kjellstrand/bst/test/TestBSTInsert.java
+++ BST/test/se/kjellstrand/bst/test/TestBSTInsert.java
@@ -20,6 +20,13 @@ public class TestBSTInsert {
 
     @Before
     public void setUp() throws Exception {
+        bstTree.insert(new Node(3));
+        bstTree.insert(new Node(5));
+        bstTree.insert(new Node(1));
+        bstTree.insert(new Node(4));
+        bstTree.insert(new Node(7));
+        bstTree.insert(new Node(2));
+        bstTree.insert(new Node(6));
     }
 
     @After
@@ -28,42 +35,14 @@ public class TestBSTInsert {
 
     @Test
     public void insertOneNode() {
-        bstTree.insert(new Node(1));
+        bstTree.insert(new Node(8));
         long size = bstTree.size();
-        Assert.assertEquals(1, size);
-    }
-
-    @Test
-    public void insertManyNodes1() {
-        bstTree.insert(new Node(1));
-        bstTree.insert(new Node(3));
-        bstTree.insert(new Node(5));
-        bstTree.insert(new Node(4));
-        bstTree.insert(new Node(2));
-        bstTree.insert(new Node(6));
-        bstTree.insert(new Node(7));
-
-        long size = bstTree.size();
-        Assert.assertEquals(7, size);
+        Assert.assertEquals(8, size);
     }
 
     @Test
     public void getInorder() {
-        bstTree.insert(new Node(3));
-        bstTree.insert(new Node(5));
-        bstTree.insert(new Node(7));
-        bstTree.insert(new Node(1));
-        bstTree.insert(new Node(4));
-        bstTree.insert(new Node(2));
-        bstTree.insert(new Node(6));
-
         ArrayList<Integer> list = bstTree.getInOrderListOfKeys();
-
-        StringBuilder sb = new StringBuilder();
-        for (Integer key : list) {
-            sb.append(key + ", ");
-        }
-        System.out.println(sb);
         Integer[] keys = list.toArray(new Integer[0]);
         Assert.assertArrayEquals(new Integer[] {
                 1, 2, 3, 4, 5, 6, 7
@@ -71,22 +50,9 @@ public class TestBSTInsert {
     }
 
     @Test
+
     public void getPreorder() {
-        bstTree.insert(new Node(3));
-        bstTree.insert(new Node(5));
-        bstTree.insert(new Node(1));
-        bstTree.insert(new Node(4));
-        bstTree.insert(new Node(7));
-        bstTree.insert(new Node(2));
-        bstTree.insert(new Node(6));
-
         ArrayList<Integer> list = bstTree.getPreOrderListOfKeys();
-
-        StringBuilder sb = new StringBuilder();
-        for (Integer key : list) {
-            sb.append(key + ", ");
-        }
-        System.out.println(sb);
         Integer[] keys = list.toArray(new Integer[0]);
         Assert.assertArrayEquals(new Integer[] {
                 3, 1, 2, 5, 4, 7, 6
@@ -95,19 +61,18 @@ public class TestBSTInsert {
 
     @Test
     public void find() {
-        bstTree.insert(new Node(3));
-        bstTree.insert(new Node(5));
-        bstTree.insert(new Node(1));
-        bstTree.insert(new Node(4));
-        bstTree.insert(new Node(7));
-        bstTree.insert(new Node(2));
-        bstTree.insert(new Node(6));
-
         Assert.assertEquals(1, bstTree.find(1).k);
         Assert.assertEquals(2, bstTree.find(2).k);
         Assert.assertEquals(3, bstTree.find(3).k);
         Assert.assertEquals(6, bstTree.find(6).k);
         Assert.assertEquals(7, bstTree.find(7).k);
         Assert.assertEquals(null, bstTree.find(8));
+    }
+
+    @Test
+    public void delete() {
+        Assert.assertEquals(2, bstTree.find(2).k);
+        bstTree.delete(2);
+        Assert.assertEquals(null, bstTree.find(2));
     }
 }
